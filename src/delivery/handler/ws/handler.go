98fd74fa7fe3040683b7d4e19daa0008168fa2a3
A. A. Sumitro <hello@aasumitro.id>
2022-08-20 17:42:02 +0800

    add: simple testing client

--- src/delivery/handler/ws/handler.go
+++ src/delivery/handler/ws/handler.go
@@ -2,6 +2,7 @@ package ws
 
 import (
 	"github.com/aasumitro/karlota/config"
+	"github.com/aasumitro/karlota/pkg/ws"
 	"github.com/gin-gonic/gin"
 )
 
@@ -9,4 +10,13 @@ type wsHandler struct{}
 
 func NewWsHandler(config *config.Config, router *gin.Engine) {
 	//handler := &wsHandler{}
+	m := ws.New()
+
+	router.GET("/ws", func(c *gin.Context) {
+		m.HandleRequest(c.Writer, c.Request)
+	})
+
+	m.HandleMessage(func(s *ws.Session, msg []byte) {
+		m.Broadcast(msg)
+	})
 }
